51528f48dcab26ff137bfe94221ce0503787e604
yuda wijaya prawira <ywijayaprawira@gmail.com>
2024-02-26 22:30:41 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,31 @@
 <script>
 import { defineComponent } from 'vue'
 
-const arraySort = []
+// const arraySort = []
 
-function initMap(origin = null, destination = null, totalLength) {
-  return new Promise((resolve, reject) => {
-    if (origin && destination) {
-      const request = {
-        origins: [origin],
-        destinations: [destination],
-        travelMode: google.maps.TravelMode.WALKING,
-        unitSystem: google.maps.UnitSystem.METRIC,
-        durationInTraffic: true,
-        avoidHighways: false,
-        avoidTolls: true
-      };
-      const service = new google.maps.DistanceMatrixService()
-      service.getDistanceMatrix(request).then((response) => {
-        resolve(response)
-      });
-    } else {
-      reject(new Error('Invalid data'))
-    }
-  })
-}
+// function initMap(origin = null, destination = null, totalLength) {
+//   return new Promise((resolve, reject) => {
+//     if (origin && destination) {
+//       const request = {
+//         origins: [origin],
+//         destinations: [destination],
+//         travelMode: google.maps.TravelMode.WALKING,
+//         unitSystem: google.maps.UnitSystem.METRIC,
+//         durationInTraffic: true,
+//         avoidHighways: false,
+//         avoidTolls: true
+//       };
+//       const service = new google.maps.DistanceMatrixService()
+//       service.getDistanceMatrix(request).then((response) => {
+//         resolve(response)
+//       });
+//     } else {
+//       reject(new Error('Invalid data'))
+//     }
+//   })
+// }
 
-window.initMap = initMap
+// window.initMap = initMap
 
 export default defineComponent({
   name: 'App'
